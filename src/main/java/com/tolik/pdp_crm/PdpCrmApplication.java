990bcf2ec22da0f1a1896f8a257f19d830d8f2ed
tolik <pdp@gmail.com>
2024-10-01 11:51:40 +0500

    CRM dasturi uchun package yaratildi

---
+++ src/main/java/com/tolik/pdp_crm/PdpCrmApplication.java
@@ -0,0 +1,13 @@
+package com.tolik.pdp_crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdpCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdpCrmApplication.class, args);
+    }
+
+}
