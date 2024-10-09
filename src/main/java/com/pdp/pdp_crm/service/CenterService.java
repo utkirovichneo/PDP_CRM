242fc36899034d36b3c7792d14e9f762edd46c22
wexad <sherzodchoriyev747@gmail.com>
2024-10-09 16:54:04 +0500

    I created mappers and filled them.

--- src/main/java/com/pdp/pdp_crm/service/CenterService.java
+++ src/main/java/com/pdp/pdp_crm/service/CenterService.java
@@ -3,12 +3,13 @@ package com.pdp.pdp_crm.service;
 
 import com.pdp.pdp_crm.dto.center.CenterDTO;
 import com.pdp.pdp_crm.dto.center.CenterRequestDTO;
+import com.pdp.pdp_crm.entity.Center;
 
 public interface CenterService {
 
     CenterDTO save(CenterRequestDTO dto);
 
-    CenterDTO findById(Long id);
+    Center findById(Long id);
 
 
 
--- src/main/java/com/pdp/pdp_crm/service/impl/CenterServiceImpl.java
+++ src/main/java/com/pdp/pdp_crm/service/impl/CenterServiceImpl.java
@@ -1,5 +1,7 @@
 package com.pdp.pdp_crm.service.impl;
 
+import com.pdp.pdp_crm.dto.center.CenterDTO;
+import com.pdp.pdp_crm.dto.center.CenterRequestDTO;
 import com.pdp.pdp_crm.entity.Center;
 import com.pdp.pdp_crm.exception.CenterNotFoundException;
 import com.pdp.pdp_crm.repository.CenterRepository;
@@ -11,6 +13,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CenterServiceImpl implements CenterService {
     private final CenterRepository centerRepository;
+
+    @Override
+    public CenterDTO save(CenterRequestDTO dto) {
+        return null;
+    }
 
     @Override
     public Center findById(Long centerId) {
