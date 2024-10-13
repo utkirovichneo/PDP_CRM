60da5018d387ac3fdffbf6edf511a22cb8b350bd
tolik <pdp@gmail.com>
2024-10-13 17:31:34 +0500

    CenterStudentController yozildi, API larda to'liq bo'lmagan qismlar mavjud

--- src/main/java/com/pdp/pdp_crm/service/StudentService.java
+++ src/main/java/com/pdp/pdp_crm/service/StudentService.java
@@ -1,19 +1,20 @@
 package com.pdp.pdp_crm.service;
 
-
 import com.pdp.pdp_crm.dto.student.StudentDTO;
 import com.pdp.pdp_crm.dto.student.StudentRequestDTO;
-import com.pdp.pdp_crm.entity.Student;
-
-import java.util.Optional;
+import com.pdp.pdp_crm.filter.PageableRequest;
+import org.springframework.data.domain.Page;
 
 public interface StudentService {
 
-    StudentDTO create(StudentRequestDTO dto);
+    StudentDTO save(Long centerId, StudentRequestDTO dto);
 
-    StudentDTO findById(Long id);
+    StudentDTO getStudentById(Long centerId, Long id);
 
-    StudentDTO update(Long id, StudentRequestDTO dto);
-    Optional<Student> findByIdOptional(Long id);
+    Page<StudentDTO> findAll(Long centerId, PageableRequest pageableRequest);
+
+    StudentDTO update(Long centerId, Long id, StudentRequestDTO dto);
+
+    Boolean delete(Long centerId, Long id);
 
 }
