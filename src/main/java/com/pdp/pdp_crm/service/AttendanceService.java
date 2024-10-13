9ab2cc900ed48b3fa4e26d092ececb4bec6f075e
tolik <pdp@gmail.com>
2024-10-13 17:22:55 +0500

    AttendanceService da CRUD dan voz kechildi

--- src/main/java/com/pdp/pdp_crm/service/AttendanceService.java
+++ src/main/java/com/pdp/pdp_crm/service/AttendanceService.java
@@ -1,11 +1,4 @@
 package com.pdp.pdp_crm.service;
 
-
-import com.pdp.pdp_crm.dto.attendance.AttendanceDTO;
-import com.pdp.pdp_crm.dto.attendance.AttendanceRequestDTO;
-
 public interface AttendanceService {
-    AttendanceDTO create (AttendanceRequestDTO dto);
-    AttendanceDTO findById(Long id);
-    AttendanceDTO update (Long id, AttendanceRequestDTO dto);
 }
