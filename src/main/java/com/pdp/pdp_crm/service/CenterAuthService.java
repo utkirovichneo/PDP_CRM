37d7d54f9c9d67475f9e5d11840680b37eefbc89
tolik <pdp@gmail.com>
2024-10-13 17:25:47 +0500

    CenterAuthController to'liq yozildi. CenterAuthService ga Ro'yxatdan o'tgan OWNER uchun Center qo'shish API i yozildi

--- src/main/java/com/pdp/pdp_crm/service/CenterAuthService.java
+++ src/main/java/com/pdp/pdp_crm/service/CenterAuthService.java
@@ -1,15 +1,19 @@
 package com.pdp.pdp_crm.service;
 
 
+import com.pdp.pdp_crm.dto.center.CenterDTO;
+import com.pdp.pdp_crm.dto.center.CenterRequestDTO;
 import com.pdp.pdp_crm.dto.token.RefreshTokenRequestDTO;
 import com.pdp.pdp_crm.dto.token.RefreshTokenResponseDTO;
 import com.pdp.pdp_crm.dto.user.UserRequestDTO;
 import com.pdp.pdp_crm.dto.user.UserResponseDTO;
+import com.pdp.pdp_crm.entity.User;
 import com.pdp.pdp_crm.util.AuthResponseDTO;
 
-import java.util.List;
+import java.util.Optional;
 
-public interface AuthService {
+
+public interface CenterAuthService {
 
     AuthResponseDTO login(UserRequestDTO userRequestDTO);
 
@@ -18,5 +22,9 @@ public interface AuthService {
     RefreshTokenResponseDTO refreshToken(RefreshTokenRequestDTO refreshTokenRequestDTO);
 
     UserResponseDTO me();
+
+    CenterDTO createCenter(CenterRequestDTO centerRequestDTO);
+
+    Optional<User> findById(Long id);
 
 }
