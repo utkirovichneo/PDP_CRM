e525d61c33da945f32cafaa96f0b0dcf1f9d6514
tolik <tolik.otkirovich@mail.ru>
2024-12-08 19:52:19 +0500

    fix filter problem

--- src/main/java/com/pdp/pdp_crm/repository/baserepository/BaseRepository.java
+++ src/main/java/com/pdp/pdp_crm/repository/baserepository/BaseRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
@@ -12,7 +13,7 @@ import org.springframework.lang.Nullable;
 import java.util.List;
 
 @NoRepositoryBean
-public interface BaseRepository<T extends BaseEntity, ID> extends JpaRepository<T, ID> {
+public interface BaseRepository<T extends BaseEntity, ID> extends JpaRepository<T, ID>, JpaSpecificationExecutor<T> {
 
     Page<T> findAll(@Nullable Pageable pageable);
 
